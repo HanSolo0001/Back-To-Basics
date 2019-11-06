e6b53ca1300cbe9d39e104275234686162749098
HanSolo0001 <pillarite@gmail.com>
2019-11-05 22:56:19 -0500

    Added cs, js, and html files
    Added cs, js, and html files with work that I've done so far.

---
+++ script.js
@@ -0,0 +1,39 @@
+alert("This is my test.");
+prompt("What do you think of this test?");
+
+/* var javascript = "This is Javascript";
+function javascript(i=0; i<=5; i++) {
+    document.write(javascript);
+}
+*/
+
+// This code works to produce text in own paragraph.
+function javascript() {
+    document.getElementById("javascript").innerHTML = "This is JavaScript";
+}
+
+
+
+function redButton() {
+    alert("Don't you know not to press the red button!");
+}
+
+
+/*
+function javascript() {
+    var js = "This is javascript";
+    
+}
+*/
+
+
+// This code checks if input is between 0 and 10. If it is code will run (in this case addition), if not error message occurs. 
+function number() {
+    var myNumber = document.getElementById('inputNumber').value;
+    if (myNumber >= 0 || myNumber <= 10) {
+        var result = myNumber + 5;
+        document.write(result).innerHTML;
+    } else {
+        document.write("Please input a number between 0 and 10.");
+    }
+}
