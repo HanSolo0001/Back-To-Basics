29ceddebf67e5b9e06fc94155fe436b785adef1d
HanSolo0001 <pillarite@gmail.com>
2019-11-24 23:26:04 -0500

    Updated script.js to play song automatically after song ends
    Updated script.js to play song automatically after song ends. Realized I haven't committed in awhile, so script.js was outdated. So fixed that as well. Mainly just comment other stuff out and testing my code against others that worked.

--- script.js
+++ script.js
@@ -1,6 +1,86 @@
 alert("This is my test.");
 prompt("What do you think of this test?");
 
+
+//// Corrected audio player
+//function initAudioPlayer() {
+//        var currentSong = 0;
+//        $("#audioPlayer")[0].src = $("#playlist li a")[0];
+//        $("#playlist li a").click(function(e){
+//            e.preventDefault();
+//            $("#audioPlayer")[0].src = this;
+//            $("#audioPlayer")[0].play();
+//            $("#playlist li").removeClass("current-song");
+//            currentSong = $(this).parent().index();
+//            $(this).parent.addClass("current-song");
+//        });
+//
+//        $("#audioPlayer")[0].addEventListener("ended", function(){
+//            alert("hi");    
+//        });
+//    }
+//
+//$(document).ready(function() {
+//   initAudioPlayer();
+//})
+
+/* Audio player source code for verification of mine
+function audioPlayer(){
+    var currentSong = 0;
+    $("#audioPlayer")[0].src = $("#playlist li a")[0];
+    $("#audioPlayer")[0].play();
+    $("#playlist li a").click(function(e){
+        e.preventDefault(); 
+        $("#audioPlayer")[0].src = this;
+        $("#audioPlayer")[0].play();
+        $("#playlist li").removeClass("current-song");
+            currentSong = $(this).parent().index();
+        $(this).parent().addClass("current-song");
+    });
+            
+    $("#audioPlayer")[0].addEventListener("ended", function(){
+        currentSong++;
+        if(currentSong == $("#playlist li a").length)
+                currentSong = 0;
+        $("#playlist li").removeClass("current-song");
+        $("#playlist li:eq("+currentSong+")").addClass("current-song");
+        $("#audioPlayer")[0].src = $("#playlist li a")[currentSong].href;
+        $("#audioPlayer")[0].play();
+    });
+}
+*/
+
+//// Audio player
+function audioPlayer() {
+    var currentSong = 0;
+    $("#audioPlayer")[0].src = $("#playlist li a")[0];
+    $("#audioPlayer")[0].play();
+    $("#playlist li a").click(function(e){
+        e.preventDefault();
+        $("#audioPlayer")[0].src = this;
+        $("#audioPlayer")[0].play();
+        $("#playlist li").removeClass("current-song");
+            currentSong = $(this).parent().index();
+        $(this).parent().addClass("current-song");
+    });
+    
+    $("#audioPlayer")[0].addEventListener("ended", function(){
+        currentSong++;
+        if(currentSong == $("#playlist li a").length)
+            currentSong = 0;
+        $("#playlist li").removeClass("current-song");
+        $("#playlist li:eq("+currentSong+")").addClass("current-song");
+        $("#audioPlayer")[0].src = $("#playlist li a")[currentSong].href;
+        $("#audioPlayer")[0].play();
+    });
+}
+
+$(document).ready(function() {
+    audioPlayer();
+})
+
+
+/*
 // Play audio
 function playSong() {
     moveAlong.play();
@@ -10,6 +90,7 @@ function playSong() {
 function pauseSong() {
     moveAlong.pause();
 }
+*/
 
 // code below another way to play song?
 // document.getElementById("moveAlong").play();
@@ -50,11 +131,11 @@ function javascript() {
 
 // This code checks if input is between 0 and 10. If it is code will run (in this case addition), if not error message occurs. 
 function number() {
-    var myNumber = document.getElementById('inputNumber').value;
-    if (myNumber >= 0 || myNumber <= 10) {
+    var myNumber = Number(document.getElementById('inputNumber').value);
+    if (myNumber >= 0 && myNumber <= 10) {
         var result = myNumber + 5;
-        document.write(result).innerHTML;
+        document.getElementById("myResult").textContent = result;
     } else {
-        document.write("Please input a number between 0 and 10.");
+        document.getElementById('myResult').textContent = "Please input a number between 0 and 10.";
     }
 }
